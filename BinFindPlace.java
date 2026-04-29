8c4f2877687ff24e12acea29a068bef988c0cb44
shwetakeshrwani92-max <shwetakeshrwani92@gmail.com>
2026-04-29 21:37:10 +0530

    Add files via upload

---
+++ BinFindPlace.java
@@ -0,0 +1,25 @@
+public class BinFindPlace {
+    public int searchInsert(int[] nums, int target) {
+     int start=0, end=nums.length-1;   
+     while(start<=end){
+        int mid=start+(end-start)/2;
+     if(nums[mid]==target){
+        return mid;
+     }
+     else if(nums[mid]<target){
+        start=mid+1;
+     }
+     else{
+        end=mid-1;
+     }
+    } 
+    return start;  
+    }
+    public static void main(String args[]){
+        int nums[]={1,3,5,6};
+        int target=7;
+        BinFindPlace obj=new BinFindPlace();
+        System.out.println(obj.searchInsert(nums,target));
+    }
+}
+
