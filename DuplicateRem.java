840c4749310213ac120fe2297545257d6202dc36
shwetakeshrwani92-max <shwetakeshrwani92@gmail.com>
2026-05-03 21:52:16 +0530

    Add files via upload

---
+++ DuplicateRem.java
@@ -0,0 +1,24 @@
+class DuplicateRem{
+    public int removeDuplicates(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+       int i=0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+       return i+1;
+    }
+    public static void main(String args[]){
+        int nums[]={1,1,2};
+        DuplicateRem obj=new DuplicateRem();
+        int k=obj.removeDuplicates(nums);
+        System.out.println("k="+k);
+        for(int i=0;i<k;i++){
+            System.out.println(nums[i]+" ");
+        }
+    }
+}
