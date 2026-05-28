c2792b36c09aaa9365674631ecc3b462b3778f57
shwetakeshrwani92-max <shwetakeshrwani92@gmail.com>
2026-05-28 22:19:35 +0530

    Add files via upload

---
+++ ReverseWord.java
@@ -0,0 +1,24 @@
+public class ReverseWord {
+    public String reverseWords(String s) {
+    String arr[]=s.split(" ");
+    StringBuilder res= new StringBuilder();    
+    for(int i=arr.length-1;i>=0;i--){
+        if(arr[i].length()==0){
+            continue;
+        }
+        if(res.length()==0){
+            res.append(arr[i]);
+        }
+        else{
+            res.append(" ");
+            res.append(arr[i]);
+        }
+    } 
+    return res.toString();
+    }      
+    public static void main(String args[]){
+        ReverseWord obj=new ReverseWord();
+        System.out.println(obj.reverseWords("the sky is blue"));
+    }
+}    
+
